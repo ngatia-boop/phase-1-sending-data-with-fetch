2736db5d9c7091754664738a319b173b43ba935b
ann ngatia <annngatia94@gmail.com>
2025-06-18 08:53:07 +0300

    Send data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>${data.id}</p>`; // Append the new ID
+    })
+    .catch(error => {
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
